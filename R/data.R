ce38a57c24bb707d259183a14a80dc5fa212c366
rachelrogers-gbr <rachel.rogers@huskers.unl.edu>
2024-01-23 11:00:16 -0600

    adding data raw file

--- R/data.R
+++ R/data.R
@@ -14,17 +14,3 @@
 #' }
 #' @source Jury Perception Study
 
-# validation_dataset_import <-
-#   read_csv("~/PhD Research/Firearm Demonstrative Evidence/Comment Analysis/validation_dataset.csv") %>%
-#   subset(select=c("clean_prints","page_count", "notes",
-#                   "corrected_notes", "time", "submission_time", "algorithm"))
-# latest_observation <- validation_dataset_import %>%
-#   group_by(clean_prints, page_count) %>%
-#   summarise(latest_time = max(time[time < submission_time])) %>% ungroup()
-#
-# combined_validation <- left_join(validation_dataset_import, latest_observation)
-# validation_dataset <- combined_validation %>% filter(time==latest_time) %>%
-#   subset(select=c("clean_prints","page_count", "notes",
-#                   "corrected_notes", "algorithm"))
-#
-# usethis::use_data(validation_dataset, overwrite=TRUE)
